689d9e526f0217ef469f8971df3f7d8b7d97b840
Bertrand Leclercq - Mac Book Pro <bl@ltm.fr>
2015-02-26 11:09:13 +0100

    Initial Commit

---
+++ TdOperateur/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  TdOperateur
+//
+//  Created by bleclercq on 26/02/2015.
+//  Copyright (c) 2015 LTM. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}

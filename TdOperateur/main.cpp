ccdf1772f8005fb76e49bf489fd4948844d9dd86
Bertrand Leclercq - Mac Book Pro <bl@ltm.fr>
2017-12-20 09:31:03 +0100

    commit décembre 2017

--- TdOperateur/main.cpp
+++ TdOperateur/main.cpp
@@ -1,15 +1,99 @@
 //
 //  main.cpp
 //  TdOperateur
-//
-//  Created by bleclercq on 26/02/2015.
-//  Copyright (c) 2015 LTM. All rights reserved.
-//
-
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+// 1.
+class String {
+private:
+    char * _ptr;
+    
+public:
+    String( const char * p = nullptr ) {
+        if( p != NULL ) {
+            _ptr = new char[strlen(p)+1];
+            strcpy(_ptr, p);
+        }else
+            _ptr = NULL;
+    }
+    
+    // copy ctor
+    String( const String& cs ) {
+        if( cs._ptr != NULL ) {
+            _ptr = new char[strlen(cs._ptr)+1];
+            strcpy(_ptr, cs._ptr);
+        }else
+            _ptr = NULL;
+    }
+    
+    String& operator = (const String& cs) {
+        if( this != &cs && cs._ptr != nullptr ) { 
+            if(_ptr != nullptr)
+                delete [] _ptr;
+            
+            _ptr = new char[strlen(cs._ptr)+1];
+            strcpy(_ptr, cs._ptr);
+        }
+        return *this;
+    }
+    
+    // 2.
+    operator const char *() const {
+        return _ptr;
+    }
+    
+    const char * get() const {
+        return _ptr;
+    }
+    
+    // 3. s1 += s2;
+    String& operator +=( const String& cs ) {
+        
+        if(cs._ptr == NULL)
+            return *this;
+        
+        if( _ptr == NULL ) {
+            this->_ptr = new char[strlen(cs._ptr)+1];
+            strcpy(_ptr, cs._ptr);
+        }else {
+            char * temp = new char[strlen(_ptr)+strlen(cs._ptr)+1];
+            strcpy( temp, _ptr );
+            strcat( temp, cs._ptr );
+            delete [] _ptr;
+            _ptr = temp;
+        }
+        
+        return *this;
+    }
+    
+    // 4 s1 = (s2 + s3);
+    String operator + (const String& cs ) const {
+        String temp;
+        temp += *this;
+        temp += cs;
+        return temp;
+    }
+    
+    ~String() {
+        if(_ptr) {
+            delete [] _ptr;
+            _ptr = NULL;
+        }
+    }
+};
+
+int main() {
+    String s1("test");
+    String s3;
+    s3 += s1;
+    
+    String s4("test");
+    s4 += s1;
+    
+    String s5("test");
+    String s10 = s4 + s5;
+    
+    std::cout << s1.get();
+    
     return 0;
 }
